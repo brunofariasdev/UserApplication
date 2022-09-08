a63bdc1b8b6a45ac03eb1d8f4c755a91c6abff07
BrunoFarias <brunofariasdev@outlook.com>
2022-09-08 15:56:50 -0300

    Responsive

--- FrontEnd/userapp/src/views/Home.vue
+++ FrontEnd/userapp/src/views/Home.vue
@@ -6,10 +6,15 @@
     <ModalConfirmed v-if="confirmeLogoff" @close="swithLogoff" @confirmed="Logoff" :text="'Deseja Sair'" />
     <div class="overflow-x-auto">
         <div
-            class="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
+            class="min-w-screen min-h-screen bg-gray-100 flex items-start justify-center bg-gray-100 font-sans overflow-hidden">
             <div class="w-full lg:w-5/6">
                 <div>
                     <div class="flex justify-end w-full items-end mb-1">
+                        <button
+                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-white-700 rounded"
+                            type="button" data-modal-toggle="defaultModal" @click="swithCreate">
+                            Adicionar Usuário
+                        </button>
                         <button @click="swithLogoff"
                             class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-white-700 rounded">
                             Sair
@@ -18,15 +23,10 @@
                     <table class="min-w-max w-full table-auto">
                         <thead>
                             <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                                <th class="py-3 px-6 text-left">Nome</th>
-                                <th class="py-3 px-6 text-left">Email</th>
-                                <th class="py-3 px-6 text-center">Ações</th>
+                                <th class="py-3 px-6 text-left ">Nome</th>
+                                <th class="py-3 px-6 text-left" id="table">Email</th>
                                 <th class="py-3 px-6 text-center">
-                                    <button
-                                        class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                        type="button" data-modal-toggle="defaultModal" @click="swithCreate">
-                                        Adicionar Usuario
-                                    </button>
+
                                 </th>
 
                             </tr>
@@ -43,14 +43,14 @@
                                             Usuário</span>
                                     </div>
                                 </td>
-                                <td class="py-3 px-6 text-center">
+                                <td class="py-3 px-6 text-center hidden md:block">
                                     <div class="flex items-center">
                                         <span>{{ item.email }}</span>
                                     </div>
                                 </td>
                                 <td class="py-3 px-6 text-center">
                                     <div class="flex item-center justify-center">
-                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer"
+                                        <div class="w-5 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer"
                                             @click="EditUser(item.id)">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 stroke="currentColor">
@@ -59,7 +59,7 @@
                                             </svg>
                                         </div>
                                         <div v-if="item.id != LoggedUser.id"
-                                            class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer"
+                                            class="w-5 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer"
                                             @click="setUserDelete(item.id)">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 stroke="currentColor">
@@ -147,3 +147,12 @@ export default {
     components: { CreatedModal, EditModal, ModalConfirmed, ModalConfirmed }
 }
 </script>
+
+
+<style scoped>
+@media (max-width: 768px) {
+    #table {
+        display: none;
+    }
+}
+</style>
